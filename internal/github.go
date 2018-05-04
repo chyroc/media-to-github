0c87ca541e891843c0ee55867033b0f412ea8ca6
Chyroc <chen_yunpeng@foxmail.com>
2018-05-04 15:27:59 +0800

    init

---
+++ internal/github.go
@@ -0,0 +1,86 @@
+package internal
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+	"encoding/json"
+	"io"
+)
+
+func getSHA(repo, token, path string) string {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://api.github.com/repos/%s/contents/%s", repo, path), nil)
+	if err != nil {
+		return ""
+	}
+	req.Header.Set("Authorization", "token "+token)
+
+	client := &http.Client{Timeout: time.Second * 10}
+	resp, err := client.Do(req)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		return ""
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+
+	var sha struct {
+		SHA string `json:"sha"`
+	}
+	err = json.Unmarshal(body, &sha)
+	if err != nil {
+		return ""
+	}
+
+	return sha.SHA
+}
+
+func Upload(repo, token, path string, content []byte) error {
+	sha := getSHA(repo, token, path)
+	var data io.Reader
+	if sha == "" {
+		data = bytes.NewReader([]byte(fmt.Sprintf(`{
+  "message": "upload %s",
+  "content": "%s"
+}`, path, base64.StdEncoding.EncodeToString(content))))
+	} else {
+		data = bytes.NewReader([]byte(fmt.Sprintf(`{
+  "message": "upload %s",
+  "content": "%s",
+  "sha": "%s"
+}`, path, base64.StdEncoding.EncodeToString(content), sha)))
+	}
+
+	req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("https://api.github.com/repos/%s/contents/%s", repo, path), data)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "token "+token)
+
+	client := &http.Client{Timeout: time.Second * 10}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf(string(body))
+	}
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"os"
+
+	"github.com/urfave/cli"
+	"github.com/Chyroc/media-to-github/internal"
+	"strings"
+	"net/http"
+	"io/ioutil"
+)
+
+var version = "v0.1.0"
+
+func main() {
+	var token string
+	var repo string
+	var file string
+	var path string
+
+	app := cli.NewApp()
+	app.Name = "media-to-github"
+	app.Version = version
+	app.HelpName = "media-to-github"
+	app.Usage = "上传图片到github"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "t, token",
+			Usage:       "github token",
+			Destination: &token,
+		},
+		cli.StringFlag{
+			Name:        "r, repo",
+			Usage:       "github repo",
+			Destination: &repo,
+		},
+		cli.StringFlag{
+			Name:        "f, file",
+			Usage:       "file path or url",
+			Destination: &file,
+		},
+		cli.StringFlag{
+			Name:        "p, path",
+			Usage:       "where file store",
+			Destination: &path,
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if token == "" || repo == "" || file == "" || path == "" {
+			return cli.ShowAppHelp(c)
+		}
+
+		var content []byte
+
+		if strings.HasPrefix(file, "http") {
+			resp, err := http.Get(file)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+
+			content, err = ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return err
+			}
+		} else {
+			content = []byte(file)
+		}
+
+		return internal.Upload(repo, token, path, content)
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
